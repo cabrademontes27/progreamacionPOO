5e9445b0271eb4944a6e14200f3c1784fd56c70a
cabrademontes27 <cabrademontes@gmail.com>
2024-08-27 13:25:42 -0600

    ejercicio3

--- src/IMC.java
+++ src/IMC.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 public class IMC {
+    @SuppressWarnings("resource")
     public static void sacarIMC()
     {
         double peso = 0.0; 
@@ -8,18 +9,36 @@ public class IMC {
         Scanner input = new Scanner(System.in);
 
 
-        
+
         System.out.println("ingresa tu peso en kg.gramos");
         peso = input.nextDouble();
         System.out.println("ahora ingresa tu altura en metros, ejempl: 1.75");
         altura = input.nextDouble();
-        double IMC = peso/(altura*altura);
+        double IMC = peso/(Math.pow(altura, 2));
+        
+        
 
-        System.out.println(" tu IMC es de " + IMC);
-
-
+        System.out.printf(" tu IMC es de %.2f " ,IMC);
 
         
+
+
+
+        if (IMC < 18.5) {
+            System.out.println("Bajo de peso");
+        } else if (IMC >= 18.5 && IMC <= 24.9) {
+            System.out.println("Normal");
+        } else if (IMC >= 25.0 && IMC <= 29.9) {
+            System.out.println("Sobrepeso");
+        } else if (IMC >= 30.0) {
+            System.out.println("Obesidad");
+        } else {
+            System.out.println("Error en el cálculo del IMC");
+        }
+        
+
+
+
 
 
 
